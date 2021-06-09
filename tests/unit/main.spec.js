9ee0d1830b008908d3ce80ec9bb5fbb2fccdafff
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-06-09 12:40:36 +0300

    test for component exists test for progress bar more than 100%

--- tests/unit/main.spec.js
+++ tests/unit/main.spec.js
@@ -1,11 +1,13 @@
 import { mount } from "@vue/test-utils";
 import App from "../../src/App.vue";
+import ProgressBar from "../../src/components/progress.vue";
 
 describe('App', ()=>{
   const wrapper = mount(App)
 
   it('should render the whole application', function () {
     expect(wrapper.html()).toContain(`Добавить рандом`)
+    expect(wrapper.findComponent(ProgressBar).exists()).toBe(true)
   })
 
  it('should create li element when push random button', done => {
@@ -16,5 +18,16 @@ describe('App', ()=>{
      done()
    })
  })
+
+  it('should be 100% progress bar', done => {
+    const button = wrapper.find('#btnRandom')
+    for (let i = 0; i < 10; i++) {
+      button.trigger('click').then(() => {
+        if (i===9) {
+          done()
+        }
+      })
+    }
+  })
 
 })
